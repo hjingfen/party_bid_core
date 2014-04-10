eac1cda011359d25cd21b306068f82110916bf8f
John Doe <johndoe@example.com>
2014-04-10 21:50:03 +0800

    bid name is dynamic

--- data_structure_1/src/BidModel.js
+++ data_structure_1/src/BidModel.js
@@ -1,15 +1,14 @@
 function Bid(current_activity){
-    this.name = "竞价1";
     this.biddings = [];
 }
 var bid = new Bid(localStorage.current_activity);
 Bid.prototype.create_new_bid = function(current_activity){
     var activities = JSON.parse(localStorage.activities);
-    var current_activity = localStorage.current_activity;
-    activities = _.map(activities, function(activity){
-        activity.name == current_activity ? activity.bids.push(bid) : '';
-        return activity;
+    var current_activity = _.find(activities, function(activity){
+        return activity['name'] == localStorage.current_activity;
     });
+    bid['name'] = "竞价"+(current_activity.bids.length+1);
+    current_activity.bids.push(bid);
     localStorage.setItem('activities',JSON.stringify(activities));
 }
 transform_bids_to_view_model = function(current_activity){
