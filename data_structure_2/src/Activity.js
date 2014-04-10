a2c727351d96546e957efeaf83fdec6670f9c594
John Doe <johndoe@example.com>
2014-04-10 13:50:45 +0800

    create activity

--- data_structure_2/src/Activity.js
+++ data_structure_2/src/Activity.js
@@ -1,5 +1,4 @@
 function Activity(activity_name){
-    this.id = localStorage.activities.length;
     this.name = activity_name;
     this.sign_ups = [];
     this.bids = [];
@@ -8,34 +7,13 @@ function Activity(activity_name){
 Activity.prototype.create = function(activity_name){
     var activities = JSON.parse(localStorage.activities);
     var activity_ids = JSON.parse(localStorage.activity_ids);
+    localStorage.current_activity = Activity.activity_id_generator();
     activities[activity_ids.length] = this;
-//    activity_ids.push()
+    activity_ids.push(JSON.stringify(Activity.activity_id_generator()));
     localStorage.activities = JSON.stringify(activities);
     localStorage.activity_ids = JSON.stringify(activity_ids);
-    localStorage.activity_id_generator = activities.length;
+    localStorage.activity_id_generator = Activity.activity_id_generator();
 }
-
-
-//localStorage.activity_id_generator = 2;
-//var activities = JSON.parse(localStorage.activities);
-//var activity_ids = JSON.parse(localStorage.activity_ids);
-//var activity_1 = {
-//    id: "0",
-//    name: "first activity",
-//    sign_ups: [],
-//    bids: [],
-//    biddings: {}
-//
-//};
-//var activity_2 = {
-//    id: "1",
-//    name: "second activity",
-//    sign_ups: [],
-//    bids: [],
-//    biddings: {}
-//}
-//activities["0"] = activity_1;
-//activities["1"] = activity_2;
-//activity_ids.push("0", "1");
-//localStorage.activities = JSON.stringify(activities);
-//localStorage.activity_ids = JSON.stringify(activity_ids);
+Activity.activity_id_generator = function(){
+    return JSON.parse(localStorage.activity_ids).length;
+}
