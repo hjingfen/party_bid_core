4b5194e19e820fb549dfabd2cce85a1cdb325f03
John Doe <johndoe@example.com>
2014-04-10 14:03:44 +0800

    code optimization

--- data_structure_2/src/Activity.js
+++ data_structure_2/src/Activity.js
@@ -7,12 +7,13 @@ function Activity(activity_name){
 Activity.prototype.create = function(activity_name){
     var activities = JSON.parse(localStorage.activities);
     var activity_ids = JSON.parse(localStorage.activity_ids);
-    localStorage.current_activity = Activity.activity_id_generator();
-    activities[activity_ids.length] = this;
-    activity_ids.push(JSON.stringify(Activity.activity_id_generator()));
+    var activity_count = Activity.activity_id_generator();
+    localStorage.current_activity = activity_count;
+    activities[activity_count] = this;
+    activity_ids.push(JSON.stringify(activity_count));
     localStorage.activities = JSON.stringify(activities);
     localStorage.activity_ids = JSON.stringify(activity_ids);
-    localStorage.activity_id_generator = Activity.activity_id_generator();
+    localStorage.activity_id_generator = activity_count+1;
 }
 Activity.activity_id_generator = function(){
     return JSON.parse(localStorage.activity_ids).length;
