893ca8b9be2c01a7cfc72f11679cc57245cfcdf2
John Doe <johndoe@example.com>
2014-04-11 10:04:32 +0800

    create activity

---
+++ data_structure_3/src/Activity.js
@@ -0,0 +1,14 @@
+function Activity(activity_name){
+    this.name = activity_name;
+}
+Activity.prototype.create = function(){
+    var activities = JSON.parse(localStorage.activities);
+    this.id = Activity.act_id();
+    localStorage.current_activity = Activity.act_id();
+    activities.push(this);
+    localStorage.setItem('activities',JSON.stringify(activities));
+}
+Activity.act_id = function(){
+    var activities = JSON.parse(localStorage.activities);
+    return JSON.stringify(activities.length);
+}
